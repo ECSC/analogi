3e69ef59ac669bedc5ba4374a7a4694f74596e94
Christian Beer <djangofett@gmx.net>
2012-07-29 17:21:49 +0200

    fixed radiobutton on index.php fixed php warning in toprare.php if database is empty include .gitignore file with my NetBeans exclusions

--- index.php
+++ index.php
@@ -36,7 +36,10 @@ if(isset($_GET['hours']) && preg_match("/^[0-9]+$/", $_GET['hours'])){
 $radiosource="";
 $radiopath="";
 $radiolevel="";
-if(isset($_GET['field']) && $_GET['field']=='path'){
+if(isset($_GET['field']) && $_GET['field']=='source'){
+    $radiosource="checked";
+}elseif(isset($_GET['field']) && $_GET['field']=='path'){
+
 	$radiopath="checked";
 }elseif(isset($_GET['field']) && $_GET['field']=='level'){
 	$radiolevel="checked";
